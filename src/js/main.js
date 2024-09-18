3d97242932a9c79382596a8df2ae28236fab6f97
Kostya <konnss9@mail.ru>
2024-09-18 20:32:37 +0300

    Added the appearance of a header when scrolling the page. Need a fix

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,20 @@ import { useBurger } from './components/burger.js';
 
 useTheme();
 useBurger();
+
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos && currentScrollPos > headerHeight) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+  prevScrollPos = currentScrollPos;
+});
+
+
+
