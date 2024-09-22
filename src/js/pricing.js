d02c3715f48d3329f2a64cf33ecdb48a2ebf892d
Kostya <konnss9@mail.ru>
2024-09-22 18:30:07 +0300

    Created pricing: hero section. And fixed

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -1,12 +1,14 @@
 /* eslint-disable prettier/prettier */
-import '/assets/scss/pricing.scss';
+import '/scss/pricing.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useHeader } from './components/header.js';
 import { useAccordionFooter } from './components/footer.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
 
 useTheme();
 useBurger();
 useHeader();
 useAccordionFooter();
+usePriceSwitcher();
