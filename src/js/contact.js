8afcd6def96963b0238beb485283fb759a61ed80
Kostya <konnss9@mail.ru>
2024-09-23 18:06:04 +0300

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -5,10 +5,12 @@ import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useHeader } from './components/header.js';
 import { usePartnersSlider } from './components/slider.js';
+import { usePhone } from './components/contact/phone.js';
 import { useAccordionFooter } from './components/footer.js';
 
 useTheme();
 useBurger();
 useHeader();
 usePartnersSlider();
+usePhone();
 useAccordionFooter();
